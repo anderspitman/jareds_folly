9ffaed4aa4f0bab7e91a1ee174397bc307ac18a4
Anders Pitman <tapitman11@gmail.com>
2018-04-10 01:40:56 -0700

    Building with webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: [ /\.vert$/, /\.frag$/ ],
+        use: 'raw-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true)
+    }),
+  ],
+};
